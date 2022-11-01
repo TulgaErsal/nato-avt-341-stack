5b246de31c5bcdce82e8fa0c2476aaf908bbc0e0
Chris Goodin <cgoodin@cavs.msstate.edu>
2022-11-01 14:28:04 -0500

    Added compile definitions for ROS1 vs ROS2

--- include/avt_341/node/clock_publisher.h
+++ include/avt_341/node/clock_publisher.h
@@ -1,6 +1,8 @@
 #ifndef CLOCK_PUBLISHER_H
 #define CLOCK_PUBLISHER_H
 
+#ifdef ROS_1
+
 #include "avt_341/node/node_proxy.h"
 #include "rosgraph_msgs/Clock.h"
 
@@ -18,4 +20,26 @@ namespace avt_341 {
 
     }
 }
+
+#else
+
+#include "avt_341/node/node_proxy.h"
+
+namespace avt_341 {
+  namespace node {
+
+    class ClockPublisher{
+    public:
+      static std::shared_ptr<ClockPublisher> make_shared(const std::string & topic_name, int qos, std::shared_ptr<NodeProxy> node);
+      ClockPublisher(const std::string topic_name, int qos, std::shared_ptr<NodeProxy> node);
+      void publish(double elapsed_time);
+    private:
+    };
+
+  }
+}
+
+
+#endif // ROS_1
+
 #endif //CLOCK_PUBLISHER_H
