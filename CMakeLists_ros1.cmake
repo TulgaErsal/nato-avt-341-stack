1c9c4a68c56af3f0666b8434bad5d6e1171112b0
Chris Goodin <cgoodin@cavs.msstate.edu>
2023-02-09 13:45:53 -0600

    Added a new speed control node that doesnt do steering control, just throttle control

--- CMakeLists_ros1.cmake
+++ CMakeLists_ros1.cmake
@@ -129,6 +129,17 @@ target_link_libraries(avt_341_control_node
   ${catkin_LIBRARIES}
 )
 
+add_executable(avt_341_speed_control_node
+  src/control/avt_341_speed_control_node.cpp
+  src/control/pure_pursuit_controller.cpp
+  src/control/pid_controller.cpp
+  src/node/node_proxy.cpp
+)
+
+target_link_libraries(avt_341_speed_control_node
+  ${catkin_LIBRARIES}
+)
+
 add_executable(speed_control_test_node
   src/control/speed_control_test_node.cpp
   src/node/node_proxy.cpp
