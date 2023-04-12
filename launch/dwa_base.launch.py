ddf3892d969bf7e933e37f387241d34485f91705
martin_naisense <martin.hirschkorn@naisense.ai>
2023-04-12 18:53:22 -0400

    Fixed speed controller parameters to disable unused features

--- launch/dwa_base.launch.py
+++ launch/dwa_base.launch.py
@@ -135,13 +135,11 @@ def generate_launch_description():
         DeclareLaunchArgument('throttle_ki', default_value='0.0', description="Throttle PID Control - integral coeff for the PID speed controller"),
         DeclareLaunchArgument('throttle_kd', default_value='0.0', description="Throttle PID Control - derivative coeff for the PID speed controller"),
 
-        DeclareLaunchArgument('time_to_max_brake', default_value='4.0', description="Time in seconds to go from 0 to maximum braking"),
-        DeclareLaunchArgument('time_to_max_throttle', default_value='3.0', description="Time in seconds to go from 0 to maximum throttle"),
+        DeclareLaunchArgument('time_to_max_brake', default_value='0.1', description="Time in seconds to go from 0 to maximum braking"),
+        DeclareLaunchArgument('time_to_max_throttle', default_value='0.1', description="Time in seconds to go from 0 to maximum throttle"),
         DeclareLaunchArgument('use_feed_forward', default_value='false', description="Set to true to use the feed-forward model in the PID throttle control"),
-        DeclareLaunchArgument('ff_a0', default_value='0.2856', description="0th order coeff in the feed-forward model"),
-        DeclareLaunchArgument('ff_a1', default_value='0.0321', description="1st order coeff in the feed-forward model"),
-        DeclareLaunchArgument('ff_a2', default_value='0.0', description="2nd order coeff in the feed-forward model"),
         DeclareLaunchArgument('max_desired_lateral_g', default_value='0.75', description="Controller will limit the speed to try to keep the lateral g-forces under this amount. In fractional units of 9.806 m/s^2"),
+        DeclareLaunchArgument('turn_off_velocity_overshoot_corrector', default_value='True', description="Controller will limit the speed to try to keep the lateral g-forces under this amount. In fractional units of 9.806 m/s^2"),
 
         OpaqueFunction(function=evaluate_waypoint_parameters),
 
@@ -212,7 +210,10 @@ def generate_launch_description():
                 'throttle_kp': launch.substitutions.LaunchConfiguration('throttle_kp'),
                 'throttle_ki': launch.substitutions.LaunchConfiguration('throttle_ki'),
                 'throttle_kd': launch.substitutions.LaunchConfiguration('throttle_kd'),
+                'use_feed_forward': launch.substitutions.LaunchConfiguration('use_feed_forward'),
+                'time_to_max_throttle': launch.substitutions.LaunchConfiguration('time_to_max_throttle'),
                 'time_to_max_brake': launch.substitutions.LaunchConfiguration('time_to_max_brake'),
+                'turn_off_velocity_overshoot_corrector': launch.substitutions.LaunchConfiguration('turn_off_velocity_overshoot_corrector'),
                 'max_desired_lateral_g': launch.substitutions.LaunchConfiguration('max_desired_lateral_g'),
             }],
         ),
