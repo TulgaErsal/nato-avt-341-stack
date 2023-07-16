268bd205691e4573fb0ee67c7aa3e57ab2f63bf0
Stefan Wapnick <stefanwapnick@outlook.com>
2023-07-16 18:27:33 -0400

    Added costmap_clearing_method.cpp to lib sources in ros1 cmake.

--- avt_341/CMakeLists_ros1.cmake
+++ avt_341/CMakeLists_ros1.cmake
@@ -313,6 +313,7 @@ src/control/pid_controller.cpp
 src/control/pure_pursuit_controller.cpp
 src/perception/elevation_grid.cpp
 src/perception/point_cloud_generator.cpp
+src/perception/costmap_clearing_method.cpp
 src/planning/local/spline_path.cpp
 src/planning/local/spline_planner.cpp
 src/planning/local/spline_plotter.cpp
