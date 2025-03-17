b2c34c80a8e4ae0773776e101e451358360ec6f2
Marius Thoresen <marius.thoresen@ffi.no>
2025-03-17 20:05:47 +0100

    Updated to default ROS camera calibration format

--- avt_341/src/perception/calibration_tf_publisher/calibration_tf_publisher.cpp
+++ avt_341/src/perception/calibration_tf_publisher/calibration_tf_publisher.cpp
@@ -5,27 +5,32 @@
 
 std::shared_ptr<avt_341::node::NodeProxy> n = nullptr;
 
-avt_341::msg::TransformStamped parseCalibrationFile(const std::string& filename) {
+avt_341::msg::TransformStamped parseCalibrationFile(const std::string& filename, const std::string& lidar_frame, const std::string& camera_frame) {
   YAML::Node config = YAML::LoadFile(filename);
+  if (!config) {
+    std::cerr << "Failed to load YAML file: " << filename << std::endl;
+    throw std::runtime_error("Failed to load YAML file");
+  }
 
-  // Extract transformation matrix
-  auto T = config["cam0"]["T_cam_lidar"];
+  auto T_data = config["transformation_lidar_cam_matrix"]["data"];
 
   avt_341::msg::TransformStamped transform_stamped;
   transform_stamped.header.stamp = n->get_stamp();
-  transform_stamped.header.frame_id = "os_lidar"; // Change to the correct frame
-  transform_stamped.child_frame_id = "flir_camera"; // Change to the correct frame
+  transform_stamped.header.frame_id = lidar_frame;
+  transform_stamped.child_frame_id = camera_frame;
 
   // Set translation
-  transform_stamped.transform.translation.x = T[0][3].as<double>();
-  transform_stamped.transform.translation.y = T[1][3].as<double>();
-  transform_stamped.transform.translation.z = T[2][3].as<double>();
+  auto transform_data = T_data.as<std::vector<float>>();
+
+  transform_stamped.transform.translation.x = transform_data[3];
+  transform_stamped.transform.translation.y = transform_data[7];
+  transform_stamped.transform.translation.z = transform_data[11];
 
   // Extract rotation matrix and convert to quaternion
-  const avt_341::msg_tf::Matrix3x3 rotation_matrix(
-    T[0][0].as<double>(), T[0][1].as<double>(), T[0][2].as<double>(),
-    T[1][0].as<double>(), T[1][1].as<double>(), T[1][2].as<double>(),
-    T[2][0].as<double>(), T[2][1].as<double>(), T[2][2].as<double>()
+  const tf2::Matrix3x3 rotation_matrix(
+      transform_data[0], transform_data[1], transform_data[2],
+      transform_data[4], transform_data[5], transform_data[6],
+      transform_data[8], transform_data[9], transform_data[10]
   );
 
   avt_341::msg_tf::Quaternion q;
@@ -47,7 +52,10 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  const avt_341::msg::TransformStamped transform_stamped = parseCalibrationFile(filename);
+  std::string lidar_frame = n->get_parameter("lidar_frame", std::string{"os_lidar"});
+  std::string camera_frame = n->get_parameter("camera_frame", std::string{"flir_optical"});
+
+  const avt_341::msg::TransformStamped transform_stamped = parseCalibrationFile(filename, lidar_frame, camera_frame);
 
   // tf2_ros::StaticTransformBroadcaster static_broadcaster;
   auto static_broadcaster = n->create_static_transform_broadcaster();
