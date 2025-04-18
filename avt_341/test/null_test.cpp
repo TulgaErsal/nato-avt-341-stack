f0e731ef77ac03be58383a7df21d423c921dd020
stefan <stefanwapnick@outlook.com>
2025-04-18 16:50:45 -0400

    Added scaffolded unit tests. Added tests to build

---
+++ avt_341/test/null_test.cpp
@@ -0,0 +1,14 @@
+
+#include <gtest/gtest.h>
+
+TEST(null_test, null_test)
+{
+    ASSERT_EQ(true, true);
+}
+
+
+int main(int argc, char ** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
