bcc6076428fc86f9b232cfa938d069a531776385
EvanVandermate <evanderm@mtu.edu>
2025-07-18 15:30:37 -0400

    Added option to use speed_control_node for speed command output

--- avt_341/src/control/speed_control/speed_control_node.cpp
+++ avt_341/src/control/speed_control/speed_control_node.cpp
@@ -61,7 +61,7 @@ void ResetCallback(avt_341::msg::StringPtr msg){
 }
 
 int main(int argc, char *argv[]){
-  auto n = avt_341::node::init_node(argc,argv,"avt_341_control_node");
+  auto n = avt_341::node::init_node(argc,argv,"avt_341_speed_control_node");
 
   auto dc_pub = n->create_publisher<avt_341::msg::Twist>("avt_341/cmd_vel",1);
 
@@ -87,8 +87,10 @@ int main(int argc, char *argv[]){
 	float throttle_coeff, time_to_max_brake;
   float throttle_kp, throttle_ki, throttle_kd;
 	std::string display, anti_windup_method;
-  float vehicle_max_steer_angle_degrees;
+  float vehicle_max_steer_angle_degrees, steering_gain, wheelbase, max_lat_g;
   double output_max, output_min;
+  bool use_speed_controller, output_steering_percent;
+  n->get_parameter("~vehicle_wheelbase", wheelbase, 2.019f);
   n->get_parameter("~vehicle_max_steer_angle_degrees", vehicle_max_steer_angle_degrees, 25.0f);
   n->get_parameter("~throttle_coefficient", throttle_coeff, 1.0f);
   n->get_parameter("~time_to_max_brake", time_to_max_brake, 4.0f);
@@ -104,6 +106,12 @@ int main(int argc, char *argv[]){
   n->get_parameter("~pid_output_min", output_min, 0.0);
   n->get_parameter("~anti_windup_method", anti_windup_method, avt_341::control::AntiWindupMethod::ResetOnSetpoint);
   n->get_parameter("~display", display, std::string("none"));
+  n->get_parameter("~use_speed_controller", use_speed_controller, true);
+  n->get_parameter("~output_steering_percent", output_steering_percent, true);
+  n->get_parameter("~max_desired_lateral_g", max_lat_g, 1.0f);
+  
+  //n->get_parameter("~steering_gain", steering_gain, 1.0f);
+  steering_gain = 1.0f;
 
   avt_341::control::PidController controller(anti_windup_method, output_min, output_max);
 
@@ -162,47 +170,69 @@ int main(int argc, char *argv[]){
     }
     else if (current_run_state==avt_341::utils::NavStackState::Active){    // active running state
       controller.SetSetpoint(desired_speed_factor*desired_speed);
-      dc.linear.x = controller.GetControlVariable(vel,dt);
+      dc.linear.x = (use_speed_controller) ? controller.GetControlVariable(vel,dt) : desired_speed_factor*desired_speed;
     }
     else if (current_run_state==avt_341::utils::NavStackState::NotInit || current_run_state==avt_341::utils::NavStackState::Stopped){
       // bring to a smooth stop and wait / idle
       controller.SetSetpoint(0.0f);
-       dc.linear.x = controller.GetControlVariable(vel,dt);
+       dc.linear.x = (use_speed_controller) ? controller.GetControlVariable(vel,dt) : 0.0f;
     }
     else if (current_run_state==avt_341::utils::NavStackState::HardShutdown){
       // bring to a hard stop and shut down
       dc.linear.x = 0.0f;
-      dc.linear.y = 1.0f;
+      dc.linear.y = (use_speed_controller) ? 1.0f : 0.0f;
       dc.angular.z = 0.0f;
       time_to_quit = true;
     }
 
     // apply the throttle scaling coefficient
-    dc.linear.x *= throttle_coeff;
-    // check braking and throttle
-    if (std::abs(dc.linear.y) > 1e-5){
-      // apply the ramp up to the brake
-      if (current_brake_value>dc.linear.y){
-        dc.linear.y = current_brake_value - brake_step;
-        if (dc.linear.y<-1.0)dc.linear.y = -1.0;
-        if (dc.linear.y>0.0)dc.linear.y = 0.0;
+    if (use_speed_controller)
+      {
+      dc.linear.x *= throttle_coeff;
+      // check braking and throttle
+      if (std::abs(dc.linear.y) > 1e-5){
+        // apply the ramp up to the brake
+        if (current_brake_value>dc.linear.y){
+          dc.linear.y = current_brake_value - brake_step;
+          if (dc.linear.y<-1.0)dc.linear.y = -1.0;
+          if (dc.linear.y>0.0)dc.linear.y = 0.0;
+        }
+        // make sure the throttle is zero when braking
+        dc.linear.x = 0.0f;
       }
-      // make sure the throttle is zero when braking
-      dc.linear.x = 0.0f;
-    }
-    // apply the throttle ramp up
-    if (dc.linear.x-current_throttle_value > max_throttle_step){
-      dc.linear.x = current_throttle_value + max_throttle_step;
+      // apply the throttle ramp up
+      if (dc.linear.x-current_throttle_value > max_throttle_step){
+        dc.linear.x = current_throttle_value + max_throttle_step;
+      }
+      // Clamp to 0<->1
+      dc.linear.x = std::max(0.0, std::min(dc.linear.x, 1.0));
     }
 
-    // Clamp the throttle effort
-    dc.linear.x = std::max(0.0, std::min(dc.linear.x, 1.0));
+    // Enforce bounds
+    if (output_steering_percent) {
+      dc.angular.z = std::max(-1.0f,std::min(1.0f,(180.0f*desired_steer_radians/3.14159265358979f)/vehicle_max_steer_angle_degrees*steering_gain));
+    }
+    else {
+      float vehicle_max_steer_angle_rad = vehicle_max_steer_angle_degrees * 3.14159265358979f / 180.0f;
+      dc.angular.z = std::max(-vehicle_max_steer_angle_rad,std::min(vehicle_max_steer_angle_rad,desired_steer_radians*steering_gain));
+    }
 
-    // publish the driving command
-    dc.angular.z = std::max(-1.0f,std::min(1.0f,(180.0f*desired_steer_radians/3.14159265358979f)/vehicle_max_steer_angle_degrees));
-    dc_pub->publish(dc);
-    current_brake_value = dc.linear.y;
-    current_throttle_value = dc.linear.x;
+    // Enforce maximum lateral acceleration
+    avt_341::msg::Twist dc_safe = dc;
+    double lat_accel_g = (vel*vel) * tan(dc_safe.angular.z) / wheelbase / 9.81;
+    if (abs(lat_accel_g) > max_lat_g) {
+      n->log_info("Lateral acceleration limit activated: %f g", lat_accel_g);
+      // Calculate maximum speed for commanded steering angle
+      dc_safe.linear.x = sqrt(abs((max_lat_g*9.81) * wheelbase / tan(dc_safe.angular.z)));
+      if (dc_safe.linear.x > dc.linear.x) dc_safe.linear.x = dc.linear.x;
+      // Calculate maximum steering angle for current speed
+      dc_safe.angular.z = atan((max_lat_g*9.81) * wheelbase / (vel*vel)) * (dc_safe.angular.z/abs(dc_safe.angular.z));
+    }
+
+    // Publish command
+    dc_pub->publish(dc_safe);
+    current_brake_value = dc_safe.linear.y;
+    current_throttle_value = dc_safe.linear.x;
 
     // break the loop when an end state is reached
     if (time_to_quit)break;
