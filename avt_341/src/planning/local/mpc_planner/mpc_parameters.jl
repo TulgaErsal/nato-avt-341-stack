30607ae44da286476df89687a7b8a9ebbe6558a1
Tulga Ersal <tersal@umich.edu>
2025-11-09 20:41:11 -0500

    added distanceToGoalAlongPath as a new MPC cost term to use instead of distanceToGoal when in following mode

--- avt_341/src/planning/local/mpc_planner/mpc_parameters.jl
+++ avt_341/src/planning/local/mpc_planner/mpc_parameters.jl
@@ -61,6 +61,11 @@ slopeThreshold = 0.2
 rmsThreshold = 0.05 # in meters
 speedAroundLargeSlopesAndRMS = 4.0 # in m/s
 
+# LogSumExp smoothing parameter for the along-path distance to goal
+# Larger values (>10) give better approximation of true minimum but may be numerically less stable
+# Smaller values (<5) give smoother but less accurate approximation
+beta = 10.0  # default provides good balance between accuracy and numerical stability
+
 # linear tire model parameters
 Caf =  -3.69554	# cornering stiffness--front axle (N/rad)
 Car =  -2.76883	# cornering stiffness-- rear axle (N/rad)
