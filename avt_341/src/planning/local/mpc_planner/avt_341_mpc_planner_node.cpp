e2ca9c2a654d5d4bfe85d6fdfac1528ae9c9433a
Tulga Ersal <tersal@umich.edu>
2025-12-15 11:04:51 -0500

    reverted the change from leader_Status to follower_status in MPC

--- avt_341/src/planning/local/mpc_planner/avt_341_mpc_planner_node.cpp
+++ avt_341/src/planning/local/mpc_planner/avt_341_mpc_planner_node.cpp
@@ -152,7 +152,7 @@ void LeaderOdomCallback(avt_341::msg::OdometryPtr msg)
     CATCH_JULIA_EXCEPTION;
 }
 
-void FollowerStatusCallback(avt_341::msg::BoolPtr msg)
+void LeaderStatusCallback(avt_341::msg::BoolPtr msg)
 {
     bool status = !(msg->data);
     jl_value_t *j_status = jl_box_bool(status);
@@ -595,7 +595,7 @@ int main(int argc, char *argv[])
     auto terrain_slope_sub = node->create_subscription<avt_341::msg::Float64>("avt_341/terrain_slope",1,TerrainSlopeCallback);
     auto terrain_rms_sub = node->create_subscription<avt_341::msg::Float64>("avt_341/terrain_rms",1,TerrainRMSCallback);
     auto leader_odom_sub = node->create_subscription<avt_341::msg::Odometry>("avt_341/leader_odometry",1,LeaderOdomCallback);
-    auto leader_status_sub = node->create_subscription<avt_341::msg::Bool>("avt_341/follower_status",1,FollowerStatusCallback);
+    auto leader_status_sub = node->create_subscription<avt_341::msg::Bool>("avt_341/leader_status",1,LeaderStatusCallback);
 
     // Register publishers
     // -------------------.
