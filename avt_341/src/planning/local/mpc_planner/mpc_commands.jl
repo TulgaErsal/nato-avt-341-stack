1dba824bb811d963695fb5258a4a48cbe62ec1d2
Tulga Ersal <tersal@umich.edu>
2026-03-23 23:18:35 -0400

    normalize the Gaussian obstacle cost by the instantaneous speed

--- avt_341/src/planning/local/mpc_planner/mpc_commands.jl
+++ avt_341/src/planning/local/mpc_planner/mpc_commands.jl
@@ -458,7 +458,7 @@ function Setup()
 		-sum(exp(-beta * ((x[j]-g1)^2 + (y[j]-g2)^2)) for j=1:n.ocp.state.pts)
 	)
 	
-	distanceToObstacles = @NLexpression(n.ocp.mdl,sum((exp(-((x[j] - Xobs_0[i])^2/(obs_r[i] + safetyMargin)^2 +(y[j] - Yobs_0[i])^2/(obs_r[i] + safetyMargin)^2))) for i=1:maxNumObs for j=2:n.ocp.state.pts))
+	distanceToObstacles = @NLexpression(n.ocp.mdl,sum((ux[j] / maxSpeed) * (exp(-((x[j] - Xobs_0[i])^2/(obs_r[i] + safetyMargin)^2 +(y[j] - Yobs_0[i])^2/(obs_r[i] + safetyMargin)^2))) for i=1:maxNumObs for j=2:n.ocp.state.pts))
 
 	deviationInYaw = @NLexpression(n.ocp.mdl, (cos(psi[2])-cos(desiredYaw))^2+(sin(psi[2])-sin(desiredYaw))^2)
 	yawAccel = @NLexpression(n.ocp.mdl, sum((ux[i] * sr[i])^2 for i=2:n.ocp.state.pts))
