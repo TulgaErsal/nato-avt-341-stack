e5d30a644b64284a62c3654e8af4a6839f9ad98d
Stefan Wapnick <stefanwapnick@outlook.com>
2026-06-17 18:17:45 -0400

    Renamed some bag topics. Added common leader odom to object tracker node for logging purposes. Added velocity estimate to tracker odom publication.

--- avt_341/src/perception/tracking/object_tracking_node.cpp
+++ avt_341/src/perception/tracking/object_tracking_node.cpp
@@ -171,6 +171,9 @@ void ObjectTrackingNode::CreatePublishers() {
     target_contacts_publisher_ =
         create_publisher<nav_msgs::msg::Path>("avt_341/target_contacts", 1);
 
+    leader_odom_publisher_ =
+        create_publisher<nav_msgs::msg::Odometry>("avt_341/odometry_estimate/leader", 1);
+
     // Integrated obstacle detector publishers.
     obstacle_bboxes_publisher_ =
         create_publisher<visualization_msgs::msg::MarkerArray>(
@@ -225,7 +228,7 @@ ObjectTracker& ObjectTrackingNode::AddOrResetTracker(
 
     auto tracker = std::make_unique<ObjectTracker>(
         this, target_class, settings_, *coord_transformer_,
-        target_contacts_publisher_);
+        target_contacts_publisher_, leader_odom_publisher_);
     ObjectTracker& tracker_ref = *tracker;
     trackers_.emplace(target_class, std::move(tracker));
     RCLCPP_INFO(get_logger(), "Created tracker for target class \"%s\".",
